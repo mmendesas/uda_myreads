a58f47d383e6772be56815d28d0ae51d1116675d
Márcio Mendes <mmendes.as@gmail.com>
2019-02-17 11:17:35 -0300

    feat: adding another bookshelfs

--- src/App.js
+++ src/App.js
@@ -6,15 +6,23 @@ import * as BooksApi from "./BooksApi";
 
 class App extends Component {
   state = {
-    books: []
+    reading: [],
+    wantToRead: [],
+    read: []
   };
 
   componentDidMount() {
-    BooksApi.getAll().then(books =>
+    BooksApi.getAll().then(books => {
+      const reading = books.filter(b => b.shelf === "currentlyReading");
+      const read = books.filter(b => b.shelf === "read");
+      const wantToRead = books.filter(b => b.shelf === "wantToRead");
+
       this.setState(() => ({
-        books
-      }))
-    );
+        wantToRead,
+        read,
+        reading
+      }));
+    });
   }
 
   render() {
@@ -24,7 +32,9 @@ class App extends Component {
           <h1>My Reads</h1>
         </header>
         <div className="list-books-content">
-          <Bookshelf title="Currently Reading" books={this.state.books} />
+          <Bookshelf title="Currently Reading" books={this.state.reading} />
+          <Bookshelf title="Want to Read" books={this.state.wantToRead} />
+          <Bookshelf title="Read" books={this.state.read} />
         </div>
       </div>
     );
